459160e03cd508166120b940cb2447bcbdad97cc
JeongSeongGil <63443366+JeongSeongGil@users.noreply.github.com>
2021-03-29 10:57:43 +0900

    Add files via upload

---
+++ CodeUp/test_1040.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class test_1040 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("%d", sc.nextLong() + sc.nextLong());
+
+	}
+}
